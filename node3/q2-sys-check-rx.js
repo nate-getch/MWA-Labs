4d8f171f7cde9955fa1608147d6762cd9a1e40ce
Natnael getachew <natwinet@gmail.com>
2017-09-09 01:20:51 -0500

    Update q2-sys-check-rx.js

--- node3/q2-sys-check-rx.js
+++ node3/q2-sys-check-rx.js
@@ -6,7 +6,7 @@ function checkSystem(){
     return new Promise(function(resolve, reject){
         if(os.totalmem < 2e6)
             reject("Program requires at least 2GB Memory"); //program requires at least 2GB memory
-        else if(os.cpus().length > 2)
+        else if(os.cpus().length < 2)
             reject("Processor not supported"); // program requires at least 2 processor
         else
             resolve('System is Chekced Successfully');
