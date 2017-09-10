18b549a977a13d4343130e95a9ef818381daa4dc
Natnael <natwinet@gmail.com>
2017-09-10 15:12:41 -0500

    added async&await function on node 4 lab along with some configurations

--- node4/app.js
+++ node4/app.js
@@ -13,6 +13,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.enable('trust proxy');
+app.enable('case sensitive routing');
+app.set('view cache', true);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- node4/routes/users.js
+++ node4/routes/users.js
@@ -3,36 +3,72 @@ var fetch = require('node-fetch');
 var Rx = require('rxjs');
 var router = express.Router();
 
-const fetchUserData = function (){
-  let d = fetch('http://jsonplaceholder.typicode.com/users/')
-  .then(function(res) {
+const apiAddr = 'http://jsonplaceholder.typicode.com/users/';
+
+const fetchUserData = function () {
+  let d = fetch(apiAddr)
+    .then(function (res) {
       return res.json();
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
   return d;
 }
 
+const fetchUserDataAsync = async function () {
+  let d;
+  try {
+    d = await fetch(apiAddr);
+  } catch (err) {
+    throw err;
+  }
+
+  return d;
+}
+
+// using async and await
+router.get('/async', function (req, res) {
+  fetchUserDataAsync()
+    .then(d => {
+      return d.json()
+    })
+    .then(d => res.render('users', {
+      userdata: d,
+      method: 'Async & Await'
+    }))
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
 // using observable
-router.get('/rx', function(req, res, next) {
+router.get('/rx', function (req, res, next) {
   Rx.Observable.fromPromise(
-    fetchUserData()
-  )
-  .subscribe(
-    d => res.render('users', { userdata: d, method: 'Observable' }) , 
-    err => console.error(err),
-    () => console.log('done')
-  )
+      fetchUserData()
+    )
+    .subscribe(
+      d => res.render('users', {
+        userdata: d,
+        method: 'Observable'
+      }),
+      err => console.error(err),
+      () => console.log('done')
+    )
 });
 
 // using promise, default fetch
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  fetch('http://jsonplaceholder.typicode.com/users/')
-  .then ( userdata => { return userdata.json() } )
-  .then( userdata => res.render('users', { userdata: userdata, method: 'Promise' }) )
-  .catch( err => console.log(err));
+router.get('/', function (req, res, next) {
+  fetch(apiAddr)
+    .then(userdata => {
+      return userdata.json()
+    })
+    .then(userdata => res.render('users', {
+      userdata: userdata,
+      method: 'Promise'
+    }))
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
