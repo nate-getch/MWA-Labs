f74ee70cf52362c2f3d2c685cfae64c694bbf3d4
Natnael <natwinet@gmail.com>
2017-09-11 02:49:24 -0500

    implemented node5 lab with express, form, csrf, validator, and session

---
+++ node5/app.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+//var randomstring = require("randomstring");
+var cookieSession = require('cookie-session');
+var validator = require('express-validator');
+
+var index = require('./routes/index');
+var newsletter = require('./routes/newsletter');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('trust proxy', 1);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(validator());
+app.use(cookieSession({
+                    name: 'session',
+                    secret: "nati",//randomstring.generate()
+                    httpOnly: true,
+                    maxAge: 30 * 60 * 1000,
+                    secure: false,
+                    overwrite: false,
+              }));
+
+app.use('/', index);
+app.use('/newsletter', newsletter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(5000, () => console.log("Listening to port 5000"));
+module.exports = app;
---
+++ node5/routes/newsletter.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
+var csrf = require('csurf'); // API refer https://github.com/expressjs/csurf
+
+// setup route middlewares 
+var csrfProtection = csrf({ cookie: true })
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+var router = express.Router();
+
+let error = null;
+
+router.post('/', csrfProtection, urlencodedParser, 
+  // validate form
+  function(req, res, next) {
+    req.checkBody('email', 'must be an email').isEmail();
+    const err = req.validationErrors(true );    
+    //console.log(err);
+
+    if(err || req.session.csrfToken !== req.body._csrf ){
+      req.session.csrfToken = req.csrfToken();
+      res.render('newsletter', {title: 'Subscribe', error:"A valid Email is Required", csrfToken: req.session.csrfToken });
+    }
+    else{
+      req.session.email = req.body.email;
+      console.log(req.body.email);
+      /*if(req.session.csrfToken == req.body._csrf ){
+        console.log("Walla");
+      }*/
+      return next();
+    }
+  
+  },
+  // save to text file and show Thanks page
+  function(req, res, next) {
+    res.redirect('/newsletter/thankyou');
+
+  }
+);
+
+router.get('/thankyou', function(req, res, next) {
+  // this page should not be accessed directly
+  if(req.session.email){
+    let email = req.session.email;
+    //destroy session
+    req.session.email = null;
+    req.session.csrfToken = null;
+    res.render('thanks', {title: 'Thank You', email });
+  }
+  else{
+    res.redirect('/newsletter');
+  }
+
+});
+
+/* GET subscribe form */
+router.get('/', csrfProtection, function(req, res, next) {
+    req.session.csrfToken = req.csrfToken();
+    res.render('newsletter', {title: 'Subscribe', error:null, csrfToken: req.session.csrfToken });
+});
+
+module.exports = router;
